dd8e6aa46fc8db32b24f0b51447346876fd121d7
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2017-06-09 13:00:26 -0700

    add addEventListener assignment

--- scripts/album.js
+++ scripts/album.js
@@ -56,30 +56,40 @@ var createSongRow = function(songNumber, songName, songLength) {
 
     return template;
 };
+// Select elements that we want to populate with text dynamically
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
 var setCurrentAlbum = function(album) {
-    // #1
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-
-    // #2
+    // Assign values to each part of the album (text, image)
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
     albumReleaseInfo.firstChild.nodeValue = album.year + " " + album.label;
     albumImage.setAttribute('src', album.albumArtUrl);
 
-    // #3
+    // Clear contents of album song list container
     albumSongList.innerHTML = " ";
 
-    // #4
+    // Build list of songs from album JavaScript object
     for (var i = 0; i < album.songs.length; i++) {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
     }
 };
 
 window.onload = function() {
-    setCurrentAlbum(albumMartin);
+    setCurrentAlbum(albumPicasso);
+
+    albums = [albumPicasso, albumMarconi, albumMartin];
+    index = 0;
+    albumImage.addEventListener("click", function(event) {
+        setCurrentAlbum(albums[index]);
+        index++;
+        if(index == albums.length) {
+          index = 0;
+        }
+
+    });
 };
