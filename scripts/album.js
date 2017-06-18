e1764bc6c18642c8325565f55cac4561d3af86f9
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2017-06-18 13:57:35 -0700

    findParentByClassName function

--- scripts/album.js
+++ scripts/album.js
@@ -113,3 +113,14 @@ window.onload = function() {
 
     });
 };
+
+var findParentByClassName = function(element, targetClass) {
+
+  if (element) {
+    var currentParent = element.parentElement;
+    while(currentParent.className !== targetClass && currentParent.className !== null) {
+      currentParent = currentParent.parentElement;
+    }
+    return currentParent;
+  }
+};
