7bb7a304bc5bcfa54660cbf0e56a980fc484aa28
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2017-06-18 14:38:43 -0700

    getSongItem

--- scripts/album.js
+++ scripts/album.js
@@ -124,3 +124,21 @@ var findParentByClassName = function(element, targetClass) {
     return currentParent;
   }
 };
+
+var getSongItem = function(element) {
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+    }  
+};
