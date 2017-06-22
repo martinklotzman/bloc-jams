b882ddf4ff07ae8e4138fff205002b2de1c12ec3
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2017-06-22 13:19:18 -0700

    update the code in mouseover listener

--- scripts/album.js
+++ scripts/album.js
@@ -145,6 +145,12 @@ window.onload = function() {
 
       if(event.target.parentElement.className === 'album-view-song-item') {
         event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+
+        var songItem = getSongItem(event.target);
+
+        if(songItem.getAttribute('song-item-number') !== currentlyPlayingSong) {
+          songItem.innerHTML = playButtonTemplate;
+        }
       }
 
     });
