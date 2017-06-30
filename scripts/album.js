6269f7922085336ec7f1b7932387d85da274e5bc
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2017-06-29 19:42:05 -0700

    code cleanup

--- scripts/album.js
+++ scripts/album.js
@@ -56,13 +56,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 
     return $(template);
 };
-// Select elements that we want to populate with text dynamically
-/*var albumTitle = document.getElementsByClassName('album-view-title')[0];
-var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-var albumImage = document.getElementsByClassName('album-cover-art')[0];
-var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-*/
+
 
 var $albumTitle = $('.album-view-title');
 var $albumArtist = $('.album-view-artist');
@@ -72,12 +66,7 @@ var $albumSongList = $('.album-view-song-list');
 
 
 var setCurrentAlbum = function(album) {
-    // Assign values to each part of the album (text, image)
-    /*albumTitle.firstChild.nodeValue = album.title;
-    albumArtist.firstChild.nodeValue = album.artist;
-    albumReleaseInfo.firstChild.nodeValue = album.year + " " + album.label;
-    albumImage.setAttribute('src', album.albumArtUrl);
-    */
+
     $albumTitle.text(album.title);
     $albumArtist.text(album.artist);
     $albumReleaseInfo.text(album.year + ' ' + album.label);
