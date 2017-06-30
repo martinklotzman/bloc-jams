3b274defab43de0dc18b4c94a36794bbe1b21b45
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2017-06-30 11:25:12 -0700

    move albums to fixture.js

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+  title: "The Colors",
+  artist: "Pablo Picasso",
+  label: "Cubism",
+  year: "1881",
+  albumArtUrl: "assets/images/album_covers/01.png",
+  songs: [
+      { title: "Blue", duration: "4:26" },
+      { title: "Green", duration: "3:14" },
+      { title: "Red", duration: "5:01" },
+      { title: "Pink", duration: "3:21" },
+      { title: "Magenta", duration: "2:15" }
+  ]
+};
+
+var albumMarconi = {
+  title: "The Telephone",
+  artist: "Guglielmo Marconi",
+  label: "EM",
+  year: "1909",
+  albumArtUrl: "assets/images/album_covers/20.png",
+  songs: [
+      { title: "Hello, Operator?", duration: "1:01" },
+      { title: "Ring, ring, ring", duration: "5:01" },
+      { title: "Fits in your pocket", duration: "3:21" },
+      { title: "Can you hear me now?", duration: "3:14" },
+      { title: "Wrong phone number", duration: "2:15" }
+  ]
+};
+
+var albumMartin = {
+  title: "Learning to Program",
+  artist: "Martin Klotzman",
+  label: "Tech World Struggles",
+  year: "2017",
+  albumArtUrl: "assets/images/album_covers/21.png",
+  songs: [
+      { title: "Defintions using definiton", duration: "4:11" },
+      { title: "Just when you think you understand", duration: "2:11" },
+      { title: "A million computer tabs", duration: "3:56" },
+      { title: "Seeing the light", duration: "1:44" },
+      { title: "Nvm still far away", duration: "9:12" }
+  ]
+};
