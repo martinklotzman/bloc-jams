b8cc2f8b5d03ff38292c55e5aa0a189ed41da79d
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2017-07-05 20:18:12 -0700

    wrap song in a buzz object

--- scripts/album.js
+++ scripts/album.js
@@ -1,6 +1,11 @@
 var setSong = function(songNumber) {
     currentlyPlayingSongNumber = parseInt(songNumber);
     currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+
+    currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+        formats: [ 'mp3' ],
+        preload: true
+    });
 };
 
 var getSongNumberCell = function(number) {
@@ -166,6 +171,7 @@ var playerBarPauseButton = '<span class="ion-pause"></span>';
 var currentAlbum = null;
 var currentlyPlayingSongNumber = null;
 var currentSongFromAlbum = null;
+var currentSoundFile = null;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
